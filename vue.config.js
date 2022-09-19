319d21efb21868df32989b3e3eb2a483c2d7eb1e
guyouxiu <1063860931@qq.com>
2022-09-19 22:44:44 +0800

    chore(custom): 修改了serve,端口号，

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    // 端口号
+    port :7777,
+    // 主机号
+    host:'localhost',
+    // 是否自动打开浏览器
+    open:true,
+    // 是否打开https
+    https:false
+  }
 })
