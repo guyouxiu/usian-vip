66af1403d316fa811fde5158cec3b8c25ec34094
guyouxiu <1063860931@qq.com>
2022-09-19 23:55:52 +0800

    feat(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath:'./',
   devServer:{
     // 端口号
     port :7777,
@@ -9,6 +10,31 @@ module.exports = defineConfig({
     // 是否自动打开浏览器
     open:true,
     // 是否打开https
-    https:false
-  }
+    https:false,
+    // 开启跨域
+    proxy:{
+      // 代理名称
+      [process.env.VUE_APP_BASE_API]:{
+        target:process.env.VUE_APP_SERVER_API,
+        changeOrigin:true,
+        pathRewrite:{
+              ["^"+process.env.VUE_APP_BASE_API]:""
+            }
+      }
+
+
+
+
+      // "/gu-api":{
+      //   // 跨域地址
+      //   target:"http://localhost:3000",
+      //   // 开启跨域
+      //   changeOrigin:true,
+      //   // 路径重写
+      //   pathRewrite:{
+      //     "^/gu-api":""
+      //   }
+      // }
+    }
+  },
 })
